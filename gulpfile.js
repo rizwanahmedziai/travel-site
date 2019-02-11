1ff868848bb239aed9ba5730c7f1f17157f3b52d
Rizwan Ahmed <rizwanahmedziai@gmail.com>
2019-02-11 17:17:36 +0500

    Finished Gulp Section

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp'),
-  watch = require('gulp-watch');
+  watch = require('gulp-watch'),
+  postcss = require('gulp-postcss'),
+  autoprefixer = require('autoprefixer'),
+  cssvars = require('postcss-simple-vars'),
+  nested = require('postcss-nested');
 
 gulp.task('default', function() {
   console.log('Gulp Task found!');
@@ -8,8 +12,15 @@ gulp.task('default', function() {
 gulp.task('html', function() {
   console.log('Something Usefull with HTML!');
 });
+// Tree important concepts: gulp.src(), gulp.dest()
+// and gulp.pipe();
+
 gulp.task('styles', function() {
-  console.log('Styles Task runing here!');
+  // console.log('Styles Task runing here!');
+  return gulp
+    .src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssvars, nested, autoprefixer]))
+    .pipe(gulp.dest('./app/tmp/styles.css'));
 });
 
 gulp.task('watch', function() {
